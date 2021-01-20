78e018c2f03a1c959cc20894c67bc8428a3c0f3a
Askell <aliadadehmav@gmail.com>
2021-01-21 02:28:55 +0700

    Add Sign in page and apply react router to app

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
+import { Link } from 'react-router-native';
+
 import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: 'row',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBarBackground,
     paddingBottom: 20
   },
   text: {
+    paddingLeft: 10,
     color: theme.colors.appBarText,
     fontSize: theme.fontSizes.subheading,
-  }
+  },
 });
 
 const AppBar = () => {
   return(
     <View style={styles.container}>
-      <TouchableWithoutFeedback>
-        <Text style={styles.text}>Repositories</Text>
-      </TouchableWithoutFeedback>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <Link to='/'>
+          <Text style={styles.text}>Repositories</Text>
+        </Link>
+        <Link to='/login'>
+          <Text style={styles.text}>Sign in</Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
